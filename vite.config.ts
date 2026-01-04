c2577257056f6f440a24be2431f83747790fcf9f
dms <danielmsantos91@gmail.com>
2026-01-04 02:16:20 +0000

    generate google search needed files

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    sitemap({
+      hostname: 'https://www.jora-studio.com',
+      dynamicRoutes: ['/', '/welcome', '/services', '/portfolio', '/studio', '/contact-us'],
+      changefreq: 'weekly',
+      priority: 0.7,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
