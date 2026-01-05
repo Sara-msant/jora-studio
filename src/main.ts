4cdcd88667e1ec9b63a9ff9451565e5cd2866ce8
dms <danielmsantos91@gmail.com>
2026-01-05 18:28:39 +0000

    prep deployment

--- src/main.ts
+++ src/main.ts
@@ -15,4 +15,39 @@ app.use(router)
 app.use(i18n)
 app.use(vuetify)
 
+// Google Analytics setup
+declare global {
+  interface Window {
+    dataLayer: any[]
+    gtag: any
+  }
+}
+
+const loadGoogleAnalytics = () => {
+  const script = document.createElement('script')
+  script.async = true
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-67LH0LX1WS'
+  document.head.appendChild(script)
+
+  window.dataLayer = window.dataLayer || []
+  function gtag() {
+    window.dataLayer.push(arguments)
+  }
+  window.gtag = gtag
+  gtag('js', new Date())
+  gtag('config', 'G-67LH0LX1WS')
+}
+
+loadGoogleAnalytics()
+
+// Track page views on route changes
+router.afterEach((to) => {
+  if (window.gtag) {
+    window.gtag('event', 'page_view', {
+      page_path: to.path,
+      page_title: document.title
+    })
+  }
+})
+
 app.mount('#app')
